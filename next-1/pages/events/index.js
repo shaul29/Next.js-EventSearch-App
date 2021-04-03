3fca59ef629215dc14caaf560072474d7c8ece92
Daniel Shaul <daniel.shaul8@gmail.com>
2021-04-03 18:17:51 -0300

    Implementing serverSide rendering And StaticSite

--- next-1/pages/events/index.js
+++ next-1/pages/events/index.js
@@ -1,12 +1,12 @@
-import { getAllEvents } from '../../dummy-data'
+import { getAllEvents } from '../../helpers/api-util'
 import EventList from '../../components/events/event-list'
 import EventsSearch from '../../components/events/events-search'
 import { Fragment } from 'react'
 import { useRouter } from 'next/router'
 
-function AllEventsPage() {
+function AllEventsPage(props) {
     const router = useRouter()
-    const events = getAllEvents()
+    const { events } = props
 
     function findEventsHandler(year, month) {
         const fullPath = `/events/${year}/${month}`
@@ -21,5 +21,17 @@ function AllEventsPage() {
         </Fragment>
     )
 }
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events,
+        },
+        revalidate: 60
+    };
+}
+
 
 export default AllEventsPage
